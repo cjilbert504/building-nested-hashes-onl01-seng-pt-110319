4d6301bbf07fe990f26494f4b15c0bea0d517037
Collin Jilbert <cjilbert504@gmail.com>
2019-11-14 17:46:38 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -4,15 +4,7 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [
-        #your hashes here!
-      ]
-   },
-   :capulet => {
-      :patriarch => {name: "Lord Capulet", age: "50"},
-      :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [{
+      :hero_friends => [{
         name: "Benvolio",
         age: "17",
         attitude: "worried"
@@ -21,7 +13,13 @@ def fourth_challenge
           age: "18",
           attitude: "hot-headed"
         }
-        #your hashes here!
+      ]
+   },
+   :capulet => {
+      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine_friends => [
       ]
    }
 }
