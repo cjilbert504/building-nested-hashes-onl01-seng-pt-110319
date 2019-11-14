0e2211ab374388dcd1e5f158b6f5a6483b184f60
Collin Jilbert <cjilbert504@gmail.com>
2019-11-14 17:45:44 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -13,13 +13,13 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [{
-        name:,
-        age:,
-        attitude:
+        name: "Benvolio",
+        age: "17",
+        attitude: "worried"
         },{
-          name:,
-          age:,
-          attitude:
+          name:"Mercutio",
+          age: "18",
+          attitude: "hot-headed"
         }
         #your hashes here!
       ]
